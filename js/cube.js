d9f05da759c5b6f631cbf75be62adab3bee9d310
jhuang314 <jh3141@gmail.com>
2013-05-29 11:29:53 -0400

    got a spinning cube!

---
+++ js/cube.js
@@ -0,0 +1,34 @@
+$(function () {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    //add the cube
+    var geometry = new THREE.CubeGeometry(1,1,1);
+    var material = new THREE.MeshBasicMaterial({
+	color: 0x00ff00
+    });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+
+    function render() {
+	requestAnimationFrame(render);
+	renderer.render(scene, camera);
+
+	cube.rotation.x += 0.1;
+	cube.rotation.y += 0.1;
+    }
+    render();
+    
+
+
+
+
+
+
+
+
+});
