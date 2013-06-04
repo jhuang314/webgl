52a5ab35090acb22d87bb45869fe70b3acb2d6b6
jhuang314 <jh3141@gmail.com>
2013-06-03 21:19:07 -0400

    updated js files to use canvas renderer if browsers do not support it

--- js/torus.js
+++ js/torus.js
@@ -17,7 +17,15 @@ $(function () {
     gui.add(params, 'zscale').min(0.1).max(3).step(0.01);
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
+
+    var renderer;
+    try {
+	renderer = new THREE.WebGLRenderer();	
+    }
+    catch(e) {
+	renderer = new THREE.CanvasRenderer();		
+    }
+
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     
